07eca51530009a1c941ba422d80e9d51b866ccf6
rndm4352 <125619119+rndm4352@users.noreply.github.com>
2023-02-16 19:01:48 -0600

    Update and rename matrix.js to matriz.js

--- matriz.js
+++ matriz.js
@@ -1,14 +1,14 @@
 const columnas = 5;
 const filas = 4;
-const matrix = [];
+const matriz = [];
 
 const caracter = "*";
 
 for (var i=0; i<filas;i++){
-    matrix[i]=[];
+    matriz[i]=[];
     for (let j=0;j<columnas;j++){
-    matrix[i][j]=caracter;
+    matriz[i][j]=caracter;
     }
 }
 
-console.log(matrix);
+console.log(matriz);
