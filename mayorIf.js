8a076f7e271ae7ccfc5d977d4ca578ed8992d481
rndm4352 <125619119+rndm4352@users.noreply.github.com>
2023-02-20 19:01:07 -0600

    Create mayorIf.js

---
+++ mayorIf.js
@@ -0,0 +1,17 @@
+//"Encuentra numero mayor if";
+var num1 = 11
+var num2 = 64
+var num3 = 99
+var resultado 
+
+//Inicia comparacion num1
+if (num1>num2 && num1 > num3){
+    resultado = num1
+//Inicia comparacion num2
+}    else if (num2>num3 && num2>num1){
+    resultado = num2
+//Inicia comparacion num3
+}else {
+    resultado = num3
+}
+console.log(resultado);
