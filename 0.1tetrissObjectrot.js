e4a26487003daa167e0f77002d6645d772d7ade3
rndm4352 <125619119+rndm4352@users.noreply.github.com>
2023-02-23 08:20:12 -0600

    Update 0.1tetrissObjectrot.js

--- 0.1tetrissObjectrot.js
+++ 0.1tetrissObjectrot.js
@@ -28,3 +28,41 @@ for(let i = 1 ; i <= 2 ; i=i+1) {//triangulo (2*1)/2
     }
 
 console.log(tetrisS);
+
+
+//TETRIS S OBJECT ROTACION 
+
+var tetrisSr = [];
+const espacior = "___"
+const caracterr = "|_|"
+//var columna = 4;
+//var fila 4
+
+for(let i = 1 ; i <= 2 ; i=i+1) {//triangulo (2*1)/2
+    for(j = 2 ; j ==2 ; j=j-1) {    //espacio izquierdo
+         tetrisSr = tetrisSr + espacior; 
+    }
+    for(j = 1 ; j<= i ; j=j+1) {         
+        tetrisSr =tetrisSr + caracterr ;
+    }
+
+    tetrisSr = tetrisSr + "\n"; 
+
+}
+//fila 3
+        for(let i = 1 ; i <= 1 ; i=i+1) {
+            for(j = 2 ; j > i ; j=j-1) {
+             tetrisSr = tetrisSr + espacior; 
+            }
+            //right space
+        for(j = 0 ; j<= i ; j=j+1) {   
+            tetrisSr = tetrisSr + espacior ;
+        }
+    }
+
+while(true){
+console.log(tetrisS);
+console.log(tetrisSr);
+}
+
+
